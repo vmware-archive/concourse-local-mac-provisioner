3625c4cd3c3c8bea683a1b9f25836ab8f6786630
Walter Scarborough <wscarborough@pivotal.io>
2017-09-28 17:59:30 +0900

    Fixed path issue on remote machines

--- scripts/concourse-web.sh
+++ scripts/concourse-web.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+source ~/workspace/concourse/.env
+
 /usr/local/bin/concourse \
     web \
     --basic-auth-username pivotal \
-    --basic-auth-password $(security find-generic-password -l concourse_local -w) \
+    --basic-auth-password $CONCOURSE_PASSWORD \
     --session-signing-key /Users/pivotal/workspace/concourse/keys/session_signing_key \
     --tsa-host-key /Users/pivotal/workspace/concourse/keys/tsa_host_key \
     --tsa-authorized-keys /Users/pivotal/workspace/concourse/keys/authorized_worker_keys \
